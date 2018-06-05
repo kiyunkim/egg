c885ed4bb9a79c1e7703485842b80ed4b2db5c85
K <kiyunkim@users.noreply.github.com>
2018-06-05 17:01:03 -0400

    var update

--- kr/js/main.js
+++ kr/js/main.js
@@ -1,23 +1,10 @@
 var _game = (function (option) {
   var self = this,
     proto = _game.prototype,
-    
-    variable = option.variable,
-    variable2 = option.variable2,
-    attackButton = option.attackButton,
 
-    resources = {
-
-    },
-    player = {
-      hp: 100,
-      level: 0
-    },
-    zombies = {
-      hp: 150,      
-      count: 0,
-      level: 0 
-    };
+    resources = {},
+    upgrades = {},
+    prestige = {};
   
 
   proto.init = function () {
