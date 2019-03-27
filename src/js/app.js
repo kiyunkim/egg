1750d3f17332b9ec1c0f878f35a04d09836fc969
K <kiyunkim@users.noreply.github.com>
2019-03-27 14:00:16 -0400

    hide save buttons unless in dev environment

--- src/js/app.js
+++ src/js/app.js
@@ -14,41 +14,52 @@ function update() {
   document.querySelector('.increment-counter').innerHTML = data.increment;
 }
 
-
-
+if (ENV_DEV) {
+  console.log('Environment: Development');
+  document.querySelector('.save-buttons').classList.remove('hidden');
+  enableSave();
+} else {
+  console.log('Environment: Production');
+}
 
 
 // save
-document.getElementById('save').addEventListener('click', function(e){
-  let save = {
-    increment: data.increment
-  };
-  try {
-    var success = true;
+function enableSave(){
+  document.getElementById('save').addEventListener('click', function(e){
+    let save = {
+      increment: data.increment
+    };
     try {
-      localStorage.setItem('code-save', JSON.stringify(save));
+      var success = true;
+  
+      // try saving..
+      try {
+        localStorage.setItem('code-save', JSON.stringify(save));
+      } catch(e) {
+        success = false;
+        console.log('Error occurred while trying to save!   "'+e+'"');
+      }
+  
+      if(success) {
+        console.log('Saved successfully');
+      }
+    
     } catch(e) {
-      success = false;
-      console.log('Error occurred while trying to save!   "'+e+'"');
+      console.log('Something went wrong while trying to save');
     }
-    if(success) {
-      console.log('saved');
+  });
+  
+  // load
+  document.getElementById('load').addEventListener('click', function(e){
+    let save = JSON.parse(localStorage.getItem('code-save'));
+    data.increment = save.increment;
+    update();
+  });
+  
+  // reset
+  document.getElementById('reset').addEventListener('click', function(e){
+    if (confirm('Are you sure you want to clear your save? This will wipe out everything!')) {
+      localStorage.removeItem('code-save');
     }
-  } catch(e) {
-    console.log('Something went wrong while trying to save');
-  }
-});
-
-// load
-document.getElementById('load').addEventListener('click', function(e){
-  let save = JSON.parse(localStorage.getItem('code-save'));
-  data.increment = save.increment;
-  update();
-});
-
-// reset
-document.getElementById('reset').addEventListener('click', function(e){
-  if (confirm('Are you sure you want to clear your save? This will wipe out everything!')) {
-    localStorage.removeItem('code-save');
-  }
-});
+  });
+}
