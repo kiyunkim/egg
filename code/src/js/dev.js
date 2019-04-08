22912661ef649e0718ef7ba4eb161c511dc971d5
K <kiyunkim@users.noreply.github.com>
2019-04-08 14:41:42 -0400

    organize + start upgrades

--- code/src/js/dev.js
+++ code/src/js/dev.js
@@ -5,16 +5,19 @@ if (ENV_DEV) {
   console.log('Environment: Development');
   console.log('Version: '+VERSION);
   
-  /* unhide
-  let hidden = document.querySelectorAll('.hidden');
-  for (let i = 0; i < hidden.length; i++) {
-    hidden[i].classList.remove('hidden');
+  enableSave();
+
+  function unhide() {
+    let hidden = document.querySelectorAll('.hidden');
+    for (let i = 0; i < hidden.length; i++) {
+      hidden[i].classList.remove('hidden');
+    }
   }
 
   document.getElementById('pause').addEventListener('click', function(){
     clearInterval(interval);
   });
-  */
+
 } else {
   console.log('Environment: Production');
 }
--- code/src/js/main.js
+++ code/src/js/main.js
@@ -4,73 +4,91 @@ import '../css/main.scss';
 import './dev';
 // import {data} from './data';
 
-let data = {
-  increment: 0
-}
-const body = document.querySelector('body');
 
+// variables
+let data = {
+  increment: 0,
+  upgrades: {
+    save: false
+  }
+}
+const tick = 1000;
+const body = document.querySelector('body');
+const incrementButton = document.getElementById('increment');
+const upgradeButton = document.querySelectorAll('.upgrade');
+
+const saveName = 'code-save';
+// dev info
 const versionNumber = VERSION;
 const version = document.querySelector('#version');
 
 version.innerHTML = 'v'+versionNumber;
 
 
-
-
-const incrementButton = document.querySelector('.increment');
-
-body.addEventListener('click', function(){
-  console.log('body clicked');
-  update();
-});
-
-let links = document.querySelectorAll('a');
-console.log(links)
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener('keypress', function(e){
-    console.log('a enter')
-    if (e.keyCode === 13){
-      e.preventDefault();
-      console.log('a enter')
-    }
-  });
+// ticker
+function ticker() {
+  //update();
 }
+export let interval = setInterval(ticker, tick);
 
+// increment click
 incrementButton.onclick = function(){
-  console.log('incrementButton c');
   data.increment++;
 };
-incrementButton.addEventListener('keypress', function(e){
-  if (e.keyCode === 13) {
-    e.preventDefault();
-    console.log('incrementButton enter');
-    data.increment++;
+
+// check for upgrades
+function checkUpgrades(){
+  for (let i = 0; i < upgradeButton.length; i++) {
+    if (data.increment >= upgradeButton[i].dataset.min) {
+      upgradeButton[i].classList.remove('hidden');
+      if (data.increment >= upgradeButton[i].dataset.cost) {
+        upgradeButton[i].disabled = false;
+      }
+    }
+    else {
+      upgradeButton[i].disabled = true;
+    }
   }
+}
+
+
+// get upgrade
+for (let i = 0; i < upgradeButton.length; i++) {
+  upgradeButton[i].onclick = function(){
+    // get type of upgrade
+    let type = this.dataset.type;
+    switch (type) {
+      case 'unlock':
+        console.log(this);
+        break;
+      default:
+        console.log('not sure what type this is');
+    }
+  }
+}
+
+// update data
+body.addEventListener('click', function(){
+  update();
 });
 
-
 function update() {
-  // console.log('update');
   document.querySelector('.increment-counter').innerHTML = data.increment;
-  
-  if (data.increment >= 5) {
-    document.querySelector('.header').classList.remove('hidden');
-    enableSave();
-  }
+  checkUpgrades();
 }
 
-const tick = 1000;
-
-function ticker() {
+// init
+window.onload = function() {
+  let save = JSON.parse(localStorage.getItem(saveName));
+  if (save) {
+    data.increment = save.increment;
+  }
   update();
 }
-
-export let interval = setInterval(ticker, tick);
 
 
 // save
 export function enableSave(){
-  const saveName = 'code-save';
 
   document.getElementById('save').addEventListener('click', function(e){
     let save = {
@@ -107,6 +125,8 @@ export function enableSave(){
   document.getElementById('reset').addEventListener('click', function(e){
     if (confirm('Are you sure you want to clear your save? This will wipe out everything!')) {
       localStorage.removeItem(saveName);
+      data.increment = 0; 
+      update();
     }
   });
 }
