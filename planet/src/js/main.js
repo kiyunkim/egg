1d3f696d61e5ae6cab541d2c3ec52573d8d387ef
kiyun <kiyunkim@users.noreply.github.com>
2019-04-23 02:17:23 -0400

    i need to stop starting new projects
    to do: look up js modules..

---
+++ planet/src/js/main.js
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import '../css/main.scss';
+
+import data from './resources';
+
+export const GAME = {};
+GAME.str = 'game';
+
+console.log(GAME);
+console.log(data);
---
+++ planet/src/js/resources.js
@@ -0,0 +1,8 @@
+import * as game from './main';
+
+
+game.data = {
+  'test': 1
+};
+
+export default game;
