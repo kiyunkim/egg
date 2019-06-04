8d3be8ca34b4a17b9300f7e81e4899aa281dd6b5
K <kiyunkim@users.noreply.github.com>
2019-06-04 13:33:48 -0400

    artistdle: basic progress bar

--- artistdle/src/js/main.js
+++ artistdle/src/js/main.js
@@ -10,8 +10,9 @@ const data = {};
 // player data
 let player = {
   talent: 0,
+  pieces: {},
   skills: {},
-  boost: 0,
+  materials: {},
 };
 
 // pieces
@@ -25,6 +26,9 @@ data.pieces = {
 // skills
 data.skills = {
   drawing: {
+    name: 'drawing',
+    level: 1,
+    boost: 1
   },
   printing: {
     name: 'printing'
@@ -85,4 +89,53 @@ data.materials = {
   }
 };
 
-console.log(data);
+const tick = 100;
+
+
+// names for each action, for picking up section id
+const sketch = 'sketch';
+
+// element/selector names
+const progressBarName = 'progress-bar';
+
+// data-tag
+// data-progress, data-value, data-max
+let progressStep = 1;
+let progressInterval = 25;
+
+// start progress bar interval
+function progressInt(name) {
+  let wrapper = document.getElementById(name);
+
+  let progressBar = wrapper.querySelector('[data-name="progress-bar"');
+  let progressVal = parseInt(progressBar.getAttribute('data-value'));
+  let progressMax = parseInt(progressBar.getAttribute('data-max'));
+  let progressMeter = progressBar.querySelector('[data-name="progress-meter"]');
+  let progressText = progressBar.querySelector('[data-name="progress-amount"]');
+  let unit = '%';
+
+  var interval = setInterval(function() {
+    if (progressVal === progressMax) {
+      setTimeout(function(){
+        progressVal = 0;
+        progressMeter.style.width = 0;
+        progressText.innerHTML = 0 + unit;
+      }, 150);
+      clearInterval(interval);
+      return;
+    }
+    progressVal += progressStep;
+    progressMeter.style.width = progressVal + unit;
+    progressText.innerHTML = progressVal + unit;
+  }, progressInterval);
+}
+
+function work(name) {
+  let wrapper = document.getElementById(name);
+  let button = wrapper.querySelector('[data-button="'+ name + '"');
+
+  button.addEventListener('click', function() {
+    progressInt(name);
+  });
+}
+work(sketch);
