42e27c9370d39c6c9ae31d673fbe8a3fb100a50b
K <kiyunkim@users.noreply.github.com>
2019-07-09 14:51:28 -0400

    sharky: check for income, show cost on button

--- sharky/src/js/main.js
+++ sharky/src/js/main.js
@@ -3,7 +3,7 @@ import  '../css/main.scss';
 
 import {data} from './data';
 
-const INTERVAL = 100;
+const INTERVAL = 10;
 const VERSION = V;
 
 
@@ -19,24 +19,36 @@ const resources = {
   },
 
   tick: function() {
-    for (const item in data) {
-      const itemName = data[item].name;
-      const amount = data[item].amount;
-      const article = document.querySelector(`#${data[item].name}`);
+    for (const i in data) {
+      const item = data[i];
+      const itemName = item.name;
+      const amount = item.amount;
+      const article = document.querySelector(`#${item.name}`);
 
+      // set up article if null
       if (!article) {
-        if (data[item].amount && data[item].amount > 0) {
+        if (item.amount && item.amount > 0) {
           // set up data in sidebar
           const article = document.createElement('article');
           article.setAttribute('id', itemName);
-          article.innerHTML = `<p>${itemName}: <span class="amount">${amount}</span></p>`;
+          article.innerHTML = `<p>${itemName}: <span class="amount">${amount}</span> <span class="income"></span></p>`;
 
           document.querySelector('#data').appendChild(article);
         }
+
+      // article already exists:
       } else {
-        // or just update the amount
-        article.querySelector('.amount').innerHTML = amount;
+        // check for income
+        if (item.income) {
+          for (const k in item.income) {
+            const incomePerTick = (item.income[k] * amount) / (1000/INTERVAL);
+            data[k].amount = data[k].amount + incomePerTick;
+          }
+        }
+        // update the amount
+        article.querySelector('.amount').innerHTML = utils.num(amount);
       }
+
       
     }
   }
@@ -45,16 +57,26 @@ const resources = {
 
 const home = {
   tick: function() {
+    // update buttons
     const buttons = document.querySelectorAll('button');
     for (let i = 0; i < buttons.length; i++) {
-      const itemName = buttons[i].dataset.name;
+      const button = buttons[i];
+      const itemName = button.dataset.name;
+      const item = data[itemName];
+
       // enable button
-      if (buttons[i].disabled === true) {
-        const item = data[itemName];
-        for (const k in item.cost) {
-          if (data[k].amount >= item.cost[k]) {
-            buttons[i].disabled = false;
+      if (button.disabled === true) {
+        for (const costItem in item.cost) {
+          if (data[costItem].amount >= item.cost[costItem]) {
+            button.disabled = false;
           }
+        }
+      }
+
+      // update cost info
+      if (item.cost) {
+        for (const costItem in item.cost) {
+          button.querySelector('.cost').innerHTML = `${utils.num(item.cost[costItem])} ${costItem}`;
         }
       }
     }
@@ -62,6 +84,10 @@ const home = {
 }
 
 const utils = {
+  num: function(number) {
+    return Math.round(number);
+  },
+
   getSingular: function(name) {
     if (name.slice(-1) === 's'){
       return name.slice(0, -1);
@@ -73,22 +99,31 @@ const utils = {
     const button = document.createElement('button');
     const itemName = item.name;
 
+
+    button.innerHTML = `<p>${action} ${utils.getSingular(itemName)}</p>`;
     if (item.cost) {
       button.disabled = true;
+      button.innerHTML += `<p>(cost: <span class="cost"></span>)</p>`;
+      for (const costItem in item.cost) {
+        button.querySelector('.cost').innerHTML += `${item.cost[costItem]} ${costItem}`;
+      }
     }
-    button.innerHTML = `${action} ${utils.getSingular(itemName)}`;
     button.setAttribute('data-name', itemName);
     
     button.addEventListener('click', function() {
       if (!item.cost) {
         item.amount++;
       } else {
-        for (const k in item.cost) {
-          if (data[k].amount >= item.cost[k]) {
-            data[k].amount = data[k].amount - item.cost[k];
+        for (const costItem in item.cost) {
+          if (data[costItem].amount >= item.cost[costItem]) {
+
+            data[costItem].amount = data[costItem].amount - item.cost[costItem];
+
+            // increase the price of the item
+            item.cost[costItem] = item.cost[costItem] * (Math.pow(1.07, item.amount));
             item.amount++;
 
-            if (data[k].amount < item.cost[k]) {
+            if (data[costItem].amount < item.cost[costItem]) {
               button.disabled = true;
             }
           }
