59b2f09762b1c7887af6e4f60b04e6a14e69c3cf
K <kiyunkim@users.noreply.github.com>
2019-10-31 11:59:27 -0400

    cat: basic setup that i will probably abandon again

---
+++ cat/js/data/worlds.js
@@ -0,0 +1,17 @@
+CAT.worlds = {
+  default: {
+    name: '',
+    desc: '',
+    startingItems: {
+      foodBowl: 1,
+      waterBowl: 1
+    },
+    environment: {}, // world boosters/modifiers
+  },
+  test: {
+
+  },
+  sanctuary: {
+
+  }
+}
---
+++ cat/js/main.js
@@ -0,0 +1,14 @@
+var CAT = CAT || {};
+
+var VERSION = '0.1.0';
+var INTERVAL = (1000/10);
+
+CAT.main = {
+  init: function() {
+
+  },
+
+  tick: function() {
+    
+  }
+}
